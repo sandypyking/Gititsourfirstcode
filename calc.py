fa481f85e694a8b63bed5ac03bd9029b6ed0bb09
sandypyking <38485290+sandypyking@users.noreply.github.com>
2019-05-04 14:23:23 +0100

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,11 @@
+print ("Addition of A + B is " )
+return A + B
+print ("Substarction of A + B is ")
+retun A - B
+print ("Multiplication of A * B is ")
+return A * B
+sandeep
+print ("Divide of A / B ")
+return A / B
+..
+Santhosh
